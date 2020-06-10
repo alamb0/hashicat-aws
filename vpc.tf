a96ef5c8ef6b3e205e410a9fa3c431946a8f6218
Austin Lamb <00msi00@gmail.com>
2020-06-10 18:33:35 +0000

    update vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
-  source  = "app.terraform.io/AUSTIN-training/vpc/aws"
-  version = "1.43.0"
+  source = "app.terraform.io/AUSTIN-training/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
